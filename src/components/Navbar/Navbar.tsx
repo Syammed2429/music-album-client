e84d597ca025028c3263dd4d214eadd592cb6bca
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-14 22:10:53 +0530

    added navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,27 @@
-import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
-
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <>
             <Box>
-                <Text>Guest </Text>
-                <Text>Artist Login </Text>
+                <Flex
+                    justify='center'
+                    gap={10}
+                >
+                    <Text>
+                        <Link to='/'>
+                            Guest
+                        </Link>
+                    </Text>
+
+                    <Text>
+                        <Link to='/artist'>
+                            Artist Login
+                        </Link>
+                    </Text>
+                </Flex>
             </Box>
 
         </>
