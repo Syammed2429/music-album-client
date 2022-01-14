4715ab3047b31e527d918cc0e2d1373e8e32ec0c
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 00:42:27 +0530

    added pagination

--- src/components/Guest/Guest.tsx
+++ src/components/Guest/Guest.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, SimpleGrid, Text } from '@chakra-ui/react';
 import React, { FC, useEffect, useState } from 'react'
 
 
@@ -6,6 +6,7 @@ import React, { FC, useEffect, useState } from 'react'
 
 const Guest: FC = () => {
 
+    const [page, setPage] = useState<number>(1);
     const [albums, setAlbums] = useState<{
         _id: string | number,
         name: string,
@@ -20,15 +21,26 @@ const Guest: FC = () => {
     //Fetch the music album from the backend
     useEffect(() => {
         const getData = async () => {
-            const response = await fetch(`${BE}albums`);
+            const response = await fetch(`${BE}albums?page=${page}`);
             const results = await response.json();
-            // console.log('results:', results.albums[2].songs.length)
             setAlbums(results.albums)
 
         }
 
         getData()
-    }, [BE]);
+    }, [BE, page]);
+
+
+    //Get the a albums  particular 
+    const getById = async (e: string | number) => {
+        console.log('string:', e)
+        const response = await fetch(`${BE}albums/${e}`)
+        const results = await response.json();
+        console.log('results:', results)
+
+
+    }
+
 
     return (
         <>
@@ -47,6 +59,9 @@ const Guest: FC = () => {
                             color='gray.400'
                             boxShadow='dark-lg'
                             key={e._id}
+                            onClick={() => {
+                                getById(e._id)
+                            }}
                         >
                             <Text >Album Name : {e.name}</Text>
                             <Text>Artist Name : {e.artist_name}</Text>
@@ -54,13 +69,32 @@ const Guest: FC = () => {
                             <Text>Genre: {e.genre}</Text>
                             <Text>Year: {e.year}</Text>
                             <Text>Number of songs: {e.songs.length}</Text>
+
                         </SimpleGrid >
                     ))}
                 </Box>
             </Center>
+            <Flex
+                justify="space-around"
+
+            >
+
+                <Button
+                    onClick={() => setPage(prev => prev - 1)}
+                    disabled={page === 1}
+                >
+                    Prev
+                </Button>
+                <Button
+                    disabled={albums?.length === 0}
+                    onClick={() => setPage(prev => prev + 1)}>
+                    Next
+                </Button>
+            </Flex>
 
         </>
     )
 }
 
 export { Guest }
+
