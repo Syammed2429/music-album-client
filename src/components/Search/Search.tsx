cf985c77389ee2927621da2b519f721c8a03e1d9
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 02:13:00 +0530

    added styling for search bar

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,30 @@
 import React, { FC } from 'react'
-import { FormControl, FormHelperText, FormLabel, Input } from '@chakra-ui/react'
+import { Button, Container, Flex, FormControl, FormHelperText, FormLabel, Input } from '@chakra-ui/react'
+import { AiOutlineSearch } from 'react-icons/ai'
+
 
 const Search: FC = () => {
+
+
+
     return (
         <>
-            <FormControl>
-                <FormLabel htmlFor='email'>Search your favorite albums</FormLabel>
-                <Input id='email' type='email' />
-                <FormHelperText>We'll never share your email.</FormHelperText>
-            </FormControl>
+            <Container my={5}>
+
+                <FormControl>
+                    <FormLabel htmlFor='search'>Search your favorite albums</FormLabel>
+                    <Flex gap={2}>
+                        <Input
+                            id='search'
+                            type='search'
+                            placeholder="Search your albums..." />
+                        <Button>
+                            <AiOutlineSearch />
+                        </Button>
+                    </Flex>
+                    <FormHelperText>Search your favorite albums.</FormHelperText>
+                </FormControl>
+            </Container>
         </>
     )
 }
