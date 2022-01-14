bb5d94b823b80b8301fa3198bbef908bcd7de60c
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-14 22:45:38 +0530

    added routes

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-const Router = () => {
+
+import { Artist } from '../Artist/Artist'
+import { Guest } from '../Guest/Guest'
+import { Navbar } from '../Navbar/Navbar'
+
+const Router: FC = () => {
     return (
         <>
-
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Guest />} />
+                <Route path="/artist" element={<Artist />} />
+            </Routes>
         </>
     )
 }
