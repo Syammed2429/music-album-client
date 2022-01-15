349a942664e1b2a0a6b61807a7c4253cedd8714c
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 13:03:54 +0530

    update profile component

---
+++ src/components/UpdateProfile/UpdateProfile.jsx
@@ -0,0 +1,110 @@
+import React, { useRef, useState } from "react"
+import {
+    FormControl,
+    FormLabel,
+    Container,
+    Alert,
+    Button,
+    Input,
+    AlertIcon,
+
+
+} from '@chakra-ui/react'
+
+import { Link, useNavigate, } from "react-router-dom"
+
+import { useAuth } from "../../contexts/AuthContext"
+
+const UpdateProfile = () => {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const { currentUser, updatePassword, updateEmail } = useAuth()
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError("Passwords do not match")
+        }
+
+        const promises = []
+        setLoading(true)
+        setError("")
+
+        if (emailRef.current.value !== currentUser.email) {
+            promises.push(updateEmail(emailRef.current.value))
+        }
+        if (passwordRef.current.value) {
+            promises.push(updatePassword(passwordRef.current.value))
+        }
+
+        Promise.all(promises)
+            .then(() => {
+                navigate("/")
+            })
+            .catch(() => {
+                setError("Failed to update account")
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
+
+    return (
+        <>
+            <Container>
+                <Container>
+                    <h2 className="text-center mb-4">Update Profile</h2>
+                    {error &&
+                        <Alert status='error'>
+                            <AlertIcon />
+                            {error}
+                        </Alert>
+                    }
+                    <form onSubmit={handleSubmit}>
+
+                        <FormControl >
+                            <FormControl id="email">
+                                <FormControl>Email</FormControl>
+                                <Input
+                                    type="email"
+                                    ref={emailRef}
+                                    required
+                                    defaultValue={currentUser.email}
+                                />
+                            </FormControl>
+                            <FormControl id="password">
+                                <FormLabel>Password</FormLabel>
+                                <Input
+                                    type="password"
+                                    ref={passwordRef}
+                                    placeholder="Leave blank to keep the same"
+                                />
+                            </FormControl>
+                            <FormControl id="password-confirm">
+                                <FormLabel>Password Confirmation</FormLabel>
+                                <Input
+                                    type="password"
+                                    ref={passwordConfirmRef}
+                                    placeholder="Leave blank to keep the same"
+                                />
+                            </FormControl>
+                            <Button disabled={loading} className="w-100" type="submit">
+                                Update
+                            </Button>
+                        </FormControl>
+                    </form>
+
+                </Container>
+            </Container>
+            <div className="w-100 text-center mt-2">
+                <Link to="/dashboard">Cancel</Link>
+            </div>
+        </>
+    )
+}
+
+export { UpdateProfile }
