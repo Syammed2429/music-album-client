8bf52ded66588eb23fe6e585e29220bfc860b38a
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 08:56:55 +0530

    added context provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,17 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App';
+import { AuthContextProvider } from './contexts/AuthContext'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
