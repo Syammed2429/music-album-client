bea95c20ebdd5e0baeb17db065df756351c09b4e
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 15:29:11 +0530

    displying the searched results

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,23 +1,35 @@
-import React, { FC, useState, ChangeEvent, useEffect } from 'react'
-import { Box, Button, Container, Flex, FormControl, FormHelperText, FormLabel, Input } from '@chakra-ui/react'
-import { AiOutlineSearch } from 'react-icons/ai'
+import React, { FC, useState, ChangeEvent } from 'react'
+import { Box, Button, Container, Flex, FormControl, Text, FormHelperText, FormLabel, Input, SimpleGrid, useColorMode, Spinner } from '@chakra-ui/react'
+import { AiOutlineSearch } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 
 const Search: FC = () => {
     const [query, setQuery] = useState<string | null>(null)
-    const BE: string | undefined = process.env.REACT_APP_BACKEND
+    const [albumData, setAlbumData] = useState<{
+        _id: string | number,
+        name: string,
+        artist_name: string,
+        genre: string,
+        songs: string[],
+        year: number,
+    }[] | null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const BE: string | undefined = process.env.REACT_APP_BACKEND;
+    const navigate = useNavigate();
+    const { colorMode } = useColorMode();
 
 
-    // useEffect(() => {
     const searchAlbum = async (query: string | null) => {
+        setLoading(true)
         const response = await fetch(`${BE}albums/search?q=${query}`)
         const results = await response.json();
-        console.log('results:', results)
+        setAlbumData(results)
+        setLoading(false)
+
 
     }
-    // searchAlbum()
-    // }, [BE, query])
-
 
 
     return (
@@ -44,8 +56,39 @@ const Search: FC = () => {
                     </Flex>
                     <FormHelperText>Search your favorite albums.</FormHelperText>
                 </FormControl>
-                <Box>
-                    Hello
+                <Box
+                    boxShadow='dark-lg'
+
+                >
+                    {loading && (
+                        <Flex
+                            justify='center'
+                            alignItems='center'
+                        >
+                            <Spinner />
+                        </Flex>)}
+
+                    {albumData?.map((e) => (
+                        <SimpleGrid
+                            columns={{ sm: 2, md: 5 }}
+                            spacing='8'
+                            p='10'
+                            textAlign='center'
+                            rounded='lg'
+                            color={colorMode === 'light' ? 'black' : 'gray.400'}
+                            key={e._id}
+                            onClick={() => {
+                                navigate(`/album-details/${e._id}`)
+                            }}
+                        >
+                            <Text >Album Name : {e.name}</Text>
+                            <Text>Artist Name : {e.artist_name}</Text>
+                            <Text>Genre: {e.genre}</Text>
+                            <Text>Year: {e.year}</Text>
+                            <Text>Number of songs: {e.songs.length}</Text>
+
+                        </SimpleGrid >
+                    ))}
                 </Box>
             </Container>
         </>
