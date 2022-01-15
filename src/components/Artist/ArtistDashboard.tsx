01b047cf9ff3b9d3ed88b7615457a39c0fb6a103
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 13:02:37 +0530

    added a menu to logout and update profile

--- src/components/Artist/ArtistDashboard.tsx
+++ src/components/Artist/ArtistDashboard.tsx
@@ -1,12 +1,16 @@
-import { SimpleGrid, useColorMode, Text } from '@chakra-ui/react'
+import { SimpleGrid, useColorMode, Text, Center, Box, Flex, Spinner, Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import React, { FC, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
+import { FiChevronDown, FiLogOut } from 'react-icons/fi'
+
+
 
 import { useAuth } from '../../contexts/AuthContext'
+import { UpdateProfile } from '../UpdateProfile/UpdateProfile'
 
 
-const ArtistDashboard: FC = () => {
-    const { currentUser } = useAuth()
+const ArtistDashboard: any = () => {
+    const { currentUser, logout } = useAuth()
     const [artistAlbum, setArtistAlbum] = useState<{
         _id: string | number,
         name: string,
@@ -16,6 +20,8 @@ const ArtistDashboard: FC = () => {
         songs: string[]
 
     }[] | null>(null)
+    const [error, setError] = useState<string | null>(null)
+
     const { colorMode } = useColorMode()
     const navigate = useNavigate()
 
@@ -41,34 +47,96 @@ const ArtistDashboard: FC = () => {
 
     }, [BE, artist, artistName]);
 
-    return (
+
+    //Logout func
+    const handleLogOut = async () => {
+        setError(null)
+        try {
+            await logout()
+            window.location.href = 'http://localhost:3000/login'
+
+        } catch {
+            setError('Failed to log out')
+        }
+    }
+
+
+    return !currentUser ? <Navigate to="/login" /> : (
         <>
+
+
+            <Menu>
+                {({ isOpen }) => (
+                    <>
+                        <MenuButton
+                            px={4}
+                            py={2}
+                            transition='all 0.2s'
+                            borderRadius='md'
+                            borderWidth='1px'
+                            _hover={{ bg: 'gray.400' }}
+                            _expanded={{ bg: 'red.400' }}
+                            _focus={{ boxShadow: 'outline' }}
+                            isActive={isOpen} as={Button} rightIcon={<FiChevronDown />}>
+                            {isOpen ? 'Close' : 'Open'}
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem>Download</MenuItem>
+                            <MenuItem
+                                _hover={{ bg: 'red.400' }}
+
+                                onClick={handleLogOut}>
+                                Log out
+                                <FiLogOut style={{ marginLeft: "10px" }} />
+                            </MenuItem>
+                        </MenuList>
+                    </>
+                )}
+            </Menu>
+
+
             <Text>Welcome {artist}</Text>
 
-
-            {artistAlbum?.map((e) => (
-                <SimpleGrid
-                    columns={{ sm: 2, md: 5 }}
-                    spacing='8'
-                    p='10'
-                    textAlign='center'
-                    rounded='lg'
-                    color={colorMode === 'light' ? 'black' : 'gray.400'}
-                    boxShadow='dark-lg'
-                    key={e._id}
-                    onClick={() => {
-                        navigate(`/album-details/${e._id}`)
-                    }}
+            <Center py={10}>
+                <Box
+                    rounded='md'
 
                 >
-                    <Text >Album Name : {e.name}</Text>
-                    <Text>Artist Name : {e.artist_name}</Text>
-                    <Text>Genre: {e.genre}</Text>
-                    <Text>Year: {e.year}</Text>
-                    <Text>Number of songs: {e.songs.length}</Text>
+                    {artistAlbum === null ? (
+                        <Flex
+                            justify='center'
+                            alignItems='center'
+                        >
+                            <Spinner />
+                        </Flex>) : null}
 
-                </SimpleGrid >
-            ))}
+
+                    {artistAlbum?.map((e) => (
+                        <SimpleGrid
+                            columns={{ sm: 2, md: 5 }}
+                            spacing='8'
+                            p='10'
+                            textAlign='center'
+                            rounded='lg'
+                            color={colorMode === 'light' ? 'black' : 'gray.400'}
+                            boxShadow='dark-lg'
+                            key={e._id}
+                            onClick={() => {
+                                navigate(`/album-details/${e._id}`)
+                            }}
+
+                        >
+                            <Text >Album Name : {e.name}</Text>
+                            <Text>Artist Name : {e.artist_name}</Text>
+                            <Text>Genre: {e.genre}</Text>
+                            <Text>Year: {e.year}</Text>
+                            <Text>Number of songs: {e.songs.length}</Text>
+
+                        </SimpleGrid >
+                    ))}
+                </Box>
+            </Center>
+            {/* <UpdateProfile /> */}
 
 
         </>
