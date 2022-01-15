00eefaf2f610caed18455ca09e55eae7b5e53dac
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 10:16:06 +0530

    mapping through the data

--- src/components/Artist/ArtistDashboard.tsx
+++ src/components/Artist/ArtistDashboard.tsx
@@ -1,13 +1,26 @@
+import { SimpleGrid, useColorMode, Text } from '@chakra-ui/react'
 import React, { FC, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { useAuth } from '../../contexts/AuthContext'
 
 
 const ArtistDashboard: FC = () => {
     const { currentUser } = useAuth()
-    const [artistAlbum, setArtistAlbum] = useState<string[] | null>(null)
+    const [artistAlbum, setArtistAlbum] = useState<{
+        _id: string | number,
+        name: string,
+        artist_name: string,
+        genre: string,
+        year: number,
+        songs: string[]
+
+    }[] | null>(null)
+    const { colorMode } = useColorMode()
+    const navigate = useNavigate()
 
     const BE: string | undefined = process.env.REACT_APP_BACKEND
+
 
     //Taking the current user and splitting it to fetch the artist album
     const artistName: string = currentUser.email.split('@')[0];
@@ -30,7 +43,34 @@ const ArtistDashboard: FC = () => {
 
     return (
         <>
-            <h1>dashboard</h1>
+            <Text>Welcome {artist}</Text>
+
+
+            {artistAlbum?.map((e) => (
+                <SimpleGrid
+                    columns={{ sm: 2, md: 6 }}
+                    spacing='8'
+                    p='10'
+                    textAlign='center'
+                    rounded='lg'
+                    color={colorMode === 'light' ? 'black' : 'gray.400'}
+                    boxShadow='dark-lg'
+                    key={e._id}
+                    onClick={() => {
+                        navigate(`/album-details/${e._id}`)
+                    }}
+
+                >
+                    <Text >Album Name : {e.name}</Text>
+                    <Text>Artist Name : {e.artist_name}</Text>
+                    <Text>Artist Name : {e.artist_name}</Text>
+                    <Text>Genre: {e.genre}</Text>
+                    <Text>Year: {e.year}</Text>
+                    <Text>Number of songs: {e.songs.length}</Text>
+
+                </SimpleGrid >
+            ))}
+
 
         </>
     )
