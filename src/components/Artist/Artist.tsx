5fc6f43360528343e52551856780d55f9b64cf6e
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 13:03:10 +0530

    made private route

--- src/components/Artist/Artist.tsx
+++ src/components/Artist/Artist.tsx
@@ -1,12 +1,20 @@
-import React, { FC } from 'react';
+import React from 'react';
+import { Navigate, } from 'react-router-dom'
 
 
-import { Login } from '../Login/Login'
+import { useAuth } from '../../contexts/AuthContext'
+import { ArtistDashboard } from './ArtistDashboard';
 
-const Artist: FC = () => {
-    return (
+
+
+const Artist: any = () => {
+    const { currentUser } = useAuth()
+    console.log('currentUser:', !currentUser)
+
+
+    return !currentUser ? <Navigate to="/login" /> : (
         <>
-            <Login />
+            <ArtistDashboard />
         </>
     )
 }
